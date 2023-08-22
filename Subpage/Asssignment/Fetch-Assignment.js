9397595879c2a980adb4d4b69426537ca4c6c5c8
Rookies111 <rookiesboy111@gmail.com>
2023-08-22 14:37:33 +0700

    Add C Problem Code Display
    - Add Code Display
    - Make Project Preview redirect to new tab

--- Subpage/Asssignment/Fetch-Assignment.js
+++ Subpage/Asssignment/Fetch-Assignment.js
@@ -9,23 +9,60 @@ async function LoadMD() {
 
 // Function for display the markdown file in User repositories
 function displayMarkdown(markdown) {
-    let converter = new showdown.Converter()
+    const converter = new showdown.Converter()
     let html = converter.makeHtml(markdown)
     document.getElementById("markdown-display").innerHTML = html
 }
 
 // Function to run when first load the script
 async function LoadC() {
-    // Get markdown file from GitHub Repository
-    const markdown_url = "https://raw.githubusercontent.com/12inNe/12in/site/event/TurboC_review.md"
-    const response2 = await fetch(markdown_url)
-    const data2 = await response2.text()
-    displayC(data2)
+    const converter = new showdown.Converter()
+    const parent = document.getElementsByClassName("section")[0]
+    const template = document.getElementById("code-display-temp").content.querySelector("div")
+    for (i=1; i <= 8; i++) {
+        // Get markdown file from GitHub Repository
+        const markdown_url = `https://raw.githubusercontent.com/SkyJTx/SkyJTx.github.io/main/asset/problem_code/p${i}.c`
+        const response = await fetch(markdown_url)
+        const data = await response.text()
+        let item = document.importNode(template, true)
+        let html = converter.makeHtml(`# Problem ${i}\n____\n\`\`\`c\n${data}\n\`\`\``)
+        item.id = `problem-${i}`
+        item.innerHTML = html
+        item.style.display = "none"
+        parent.appendChild(item)
+    }
+    document.getElementById("problem-1").style.display = "block"
+    document.getElementById("page-index").value = 1
 }
 
-// Function for display the markdown file in User repositories
-function displayC(markdown) {
-    let converter = new showdown.Converter()
-    let html = converter.makeHtml(markdown)
-    document.getElementById("markdown-display").innerHTML = html
+function prev() {
+    let current = document.getElementById("page-index").value - 1
+    let displays = document.getElementsByClassName("code-display")
+    if (current-1 < 0) {return false}
+    for (i = 0; i < displays.length; i++) {
+        displays[i].style.display = "none"
+    }
+    displays[current-1].style.display = "block"
+    document.getElementById("page-index").value = current
+}
+
+function next() {
+    let current = document.getElementById("page-index").value - 1
+    let displays = document.getElementsByClassName("code-display")
+    if (current+1 >= displays.length) {return false}
+    for (i = 0; i < displays.length; i++) {
+        displays[i].style.display = "none"
+    }
+    displays[current+1].style.display = "block"
+    document.getElementById("page-index").value = current+2
+}
+
+function gotoPage() {
+    let target = document.getElementById("page-index").value
+    let displays = document.getElementsByClassName("code-display")
+    if (displays.length < target || target <= 0) {return false}
+    for (i = 0; i < displays.length; i++) {
+        displays[i].style.display = "none"
+    }
+    displays[target-1].style.display = "block"
 }
